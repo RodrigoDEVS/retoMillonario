b2f4cb5f4afc2e14d4f55acca8b0cc3786116442
riveco0120 <richardsk.vellojin@gmail.com>
2022-04-20 16:37:56 -0500

    Agregando boton para listar jugadores

---
+++ listaJugador.js
@@ -0,0 +1,10 @@
+(function(){
+
+    //Crear un nodo
+    var listar=document.createElement("input");
+    listar.setAttribute("type","button");
+    listar.setAttribute("value","Listarjugadores");
+
+    document.body.appendChild(listar);
+
+}());
